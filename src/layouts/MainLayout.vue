f97a0d5dfe8615555faea3a227450d7327546ad2
moeb98 <moeb98@yahoo.de>
2022-04-07 09:27:18 +0200

    fixed keep-alive for router-view

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -51,9 +51,11 @@
     -->
 
     <q-page-container class="bg-black">
-      <!-- keep-alive -->
-        <router-view />
-      <!-- /keep-alive -->
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
     </q-page-container>
   </q-layout>
 </template>
